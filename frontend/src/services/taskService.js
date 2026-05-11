dbd1beaee5171e6101ff563bf761114047940995
ravichandrareddy2712-bit <ravichandra.reddy2712@gmail.com>
2026-05-11 21:46:40 +0530

    fixed bug

--- frontend/src/services/taskService.js
+++ frontend/src/services/taskService.js
@@ -5,3 +5,4 @@ export const createTask = async (payload) => (await api.post("/tasks", payload))
 export const updateTask = async (taskId, payload) => (await api.patch(`/tasks/${taskId}`, payload)).data;
 export const deleteTask = async (taskId) => (await api.delete(`/tasks/${taskId}`)).data;
 export const moveTask = async (taskId, status) => (await api.patch(`/tasks/${taskId}/move`, { status })).data;
+export const updateTaskStatus = moveTask;
